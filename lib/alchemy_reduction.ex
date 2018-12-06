cc0d2bd2050e56fbe6cee8f317de7b14444f3bfe
Kevin Koltz <kevin.koltz@gmail.com>
2018-12-06 08:01:50 -0600

    Add is_reaction? function to be more descriptive

--- lib/alchemy_reduction.ex
+++ lib/alchemy_reduction.ex
@@ -9,23 +9,26 @@ defmodule AlchemyReduction do
 
   """
   @spec reduce_polymer(String.t()) :: non_neg_integer()
-  def reduce_polymer(input, polymer \\ [""], count \\ 0)
+  def reduce_polymer(input, polymers \\ [""], count \\ 0)
 
-  def reduce_polymer(<<current::bytes-size(1), rest::binary>> = input, [last | polymer], count)
+  def reduce_polymer(<<current::bytes-size(1), rest::binary>> = input, [last | polymers], count)
       when is_binary(input) do
-    if current != last && String.upcase(current) == String.upcase(last) do
-      reduce_polymer(rest, polymer, count - 1)
+    if is_reaction?(current, last) do
+      reduce_polymer(rest, polymers, count - 1)
     else
-      reduce_polymer(rest, [current, last | polymer], count + 1)
+      reduce_polymer(rest, [current, last | polymers], count + 1)
     end
   end
 
-  def reduce_polymer("", polymer, count) do
-    polymer =
-      polymer
+  def reduce_polymer("", polymers, count) do
+    polymer_chemical_composition =
+      polymers
       |> Enum.reverse()
       |> Enum.join()
 
-    {:ok, polymer, count}
+    {:ok, polymer_chemical_composition, count}
   end
+
+  defp is_reaction?(a, b) when a != b, do: String.upcase(a) == String.upcase(b)
+  defp is_reaction?(_, _), do: false
 end
